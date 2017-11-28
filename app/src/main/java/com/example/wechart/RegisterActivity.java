c78b67c9c477eca9c8cc35db9f7b2c40fe53c25f
Administrator <26537318082qq.com>
2017-11-28 22:00:18 +0800

    registerSuccess

---
+++ app/src/main/java/com/example/wechart/RegisterActivity.java
@@ -0,0 +1,56 @@
+package com.example.wechart;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextUserName;
+    private  EditText editTextPassword;
+    private String StringUserName;
+    private String StringPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        //隐藏标题栏
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+        //状态栏透明
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        Button BtnRegister = (Button)findViewById(R.id.register_btn_confirm);
+        editTextUserName = (EditText)findViewById(R.id.register_username_text);
+        editTextPassword = (EditText)findViewById(R.id.register_password_text);
+        BtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringUserName = editTextUserName.getText().toString();
+                StringPassword = editTextPassword.getText().toString();
+                Toast.makeText(RegisterActivity.this,"注册成功,点击登录",Toast.LENGTH_SHORT).show();
+                //注册一个intent存放数据
+                Intent intentSignin  = new Intent();
+                intentSignin.putExtra("returnUserName",StringUserName);
+                intentSignin.putExtra("returnPassword",StringPassword);
+                setResult(RESULT_OK,intentSignin);
+                finish();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/wechart/signInActivity.java
@@ -0,0 +1,63 @@
+package com.example.wechart;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class signInActivity extends AppCompatActivity {
+    private EditText editTextUserName;
+    private  EditText editTextPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+        //隐藏标题栏
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
+        //状态栏透明
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        //设置注册界面的跳转
+        Button BtnSignInRegister = (Button)this.findViewById(R.id.btn_register);
+        BtnSignInRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(signInActivity.this,RegisterActivity.class);
+                Toast.makeText(signInActivity.this, "欢迎注册", Toast.LENGTH_SHORT).show();
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case 1:
+                if(resultCode==RESULT_OK){
+                    editTextPassword=(EditText)findViewById(R.id.password_text);
+                    editTextUserName=(EditText)findViewById(R.id.username_text);
+                    String userName = data.getStringExtra("returnUserName");
+                    String passWord = data.getStringExtra("returnPassword");
+                    editTextUserName.setText(userName);
+                    editTextPassword.setText(passWord);
+
+                }break;
+            default:
+        }
+    }
+}
